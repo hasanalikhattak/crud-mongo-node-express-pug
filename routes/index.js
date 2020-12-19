b74227c9a1ad2cc78b960bb81365bdf905503fd5
tayyabakhtar62 <mtayyab.bscs17seecs@seecs.edu.pk>
2020-12-19 22:52:37 +0500

    added new functionality

--- routes/index.js
+++ routes/index.js
@@ -48,13 +48,11 @@ router.post('/',[
 function updateUser(req, res){
   const errors = validationResult(req);
   if(errors.isEmpty()){
-    User.findOneAndUpdate({_id: req.body._id}, req.body, {new: true}, (err, user) => {
-      if(!err) {
-        req.redirect('/users');
-      } else{
-        console.log(err);
-        
+    User.findOneAndUpdate({_id: req.body._id}, {$set:req.body}, {new: true}, (err, doc) => {
+      if (err) {
+          console.log("Updating data failed!"); 
       }
+      res.redirect('/users');
     });
   }
 }
